67ddd6eb803275a8a245c1b5e24e5e063252b86c
Muhamad Fitroh Sodikin <114828497+Afitr06@users.noreply.github.com>
2023-06-11 07:01:02 +0700

    Update add_post_page.dart

--- lib/pages/add_post_page.dart
+++ lib/pages/add_post_page.dart
@@ -20,11 +20,7 @@ class _addPostPageState extends State<addPostPage> {
           padding: EdgeInsets.symmetric(horizontal: size.width * 0.01),
           child: Column(
             children: [
-<<<<<<< HEAD
               TextFormField(), //139:54
-=======
-              TextFormField(),//139:54 (Deni)
->>>>>>> 71d8ad83b98a391c71b65ba2564159f664201c31
             ],
           ),
         ),
